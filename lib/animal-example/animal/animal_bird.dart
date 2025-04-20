664b7cfa88f24d58431ffe27c1b47e254e3ad22a
hani <hanisoft2022@gmail.com>
2025-04-20 10:31:10 +0900

    start

--- lib/animal-example/animal/animal_bird.dart
+++ lib/animal-example/animal/animal_bird.dart
@@ -6,6 +6,4 @@ import 'package:mixin_knowhow/animal-example/ability-mixin/walkable_mixin.dart';
 import 'package:mixin_knowhow/animal-example/animal/animal.dart';
 import 'package:mixin_knowhow/animal-example/skill/skill_use_stick.dart';
 
-class Bird extends Animal with FlyMixin, WalkableMixin{
-
-}
+class Bird extends Animal with FlyMixin, WalkableMixin {}
